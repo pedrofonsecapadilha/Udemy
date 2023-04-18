e71945a76b76853b8d1ad7b31801c79162f585d7
Pedro Padilha <pedrofpadilha@gmail.com>
2023-04-17 21:29:10 -0300

    seção 5 - 70

--- React-TheCompleteGuide/1-project/src/components/Expenses/Expenses.js
+++ React-TheCompleteGuide/1-project/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
 
@@ -25,7 +26,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}>
         </ExpensesFilter>
-
+        <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
         <ExpensesList items={filteredExpenses}></ExpensesList>
 
       </Card>
